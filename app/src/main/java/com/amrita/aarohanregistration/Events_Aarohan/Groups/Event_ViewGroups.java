6668376aecc6d3602a387fe9ad9e03e6691e3c98
Akhilesh Anand <akhileshmalini@gmail.com>
2017-09-20 10:36:13 +0530

    Search Added to Groups

--- app/src/main/java/com/amrita/aarohanregistration/Events_Aarohan/Groups/Event_ViewGroups.java
+++ app/src/main/java/com/amrita/aarohanregistration/Events_Aarohan/Groups/Event_ViewGroups.java
@@ -2,15 +2,20 @@ package com.amrita.aarohanregistration.Events_Aarohan.Groups;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amrita.aarohanregistration.Events_Aarohan.Event_Student_Group;
 import com.amrita.aarohanregistration.Events_Aarohan.Event_Student_Group_Count;
 import com.amrita.aarohanregistration.R;
 import com.google.firebase.database.DataSnapshot;
@@ -29,7 +34,7 @@ import java.util.ArrayList;
 
  */
 
-public class Event_ViewGroups extends AppCompatActivity {
+public class Event_ViewGroups extends AppCompatActivity implements SearchView.OnQueryTextListener {
     FirebaseDatabase database;
     ProgressBar progressBar;
     TextView title;
@@ -37,7 +42,7 @@ public class Event_ViewGroups extends AppCompatActivity {
     Event_ShowGroupsAdapter mAdapter;
     int grpSize;
 
-    ArrayList<Event_Student_Group_Count> evList;
+    ArrayList<Event_Student_Group_Count> evList,original,temp;
     
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -62,6 +67,8 @@ public class Event_ViewGroups extends AppCompatActivity {
 
         //ArrayList Initialization
         evList =new ArrayList<>();
+        original =new ArrayList<>();
+        temp =new ArrayList<>();
 
         DatabaseReference EventsRef = database.getReference("Events").child(evName).child("Groups");
 
@@ -85,6 +92,7 @@ public class Event_ViewGroups extends AppCompatActivity {
                         progressBar.setVisibility(View.INVISIBLE);
 
                     }
+                    original.addAll(evList);
                 }
             }
             @Override
@@ -104,4 +112,85 @@ public class Event_ViewGroups extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+
+
+
+
+        if(query.equals("")){
+            evList.clear();
+            evList.addAll(original);
+            mAdapter.notifyDataSetChanged();
+
+        }else {
+            temp.clear();
+            for (Event_Student_Group_Count student : evList) {
+                if (student.getStdName().toLowerCase().contains(query.toLowerCase())) {
+                    temp.add(student);
+                }
+            }
+            if (temp.size() == 0) {
+                evList.clear();
+
+                evList.addAll(original);
+                mAdapter.notifyDataSetChanged();
+
+            } else {
+                evList.clear();
+                evList.addAll(temp);
+                mAdapter.notifyDataSetChanged();
+            }
+
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        if(query.equals("")){
+            evList.clear();
+            evList.addAll(original);
+            mAdapter.notifyDataSetChanged();
+        }else {
+            temp.clear();
+
+            for (Event_Student_Group_Count student : evList) {
+                if (student.getStdName().toLowerCase().contains(query.toLowerCase())) {
+                    temp.add(student);
+                }
+            }
+            if (temp.size() == 0) {
+                evList.clear();
+
+                Toast.makeText(getApplicationContext(), "No School Found", Toast.LENGTH_SHORT).show();
+                evList.addAll(original);
+                mAdapter.notifyDataSetChanged();
+
+            } else {
+                evList.clear();
+                evList.addAll(temp);
+                mAdapter.notifyDataSetChanged();
+            }
+
+        }
+
+        return true;
+    }
+    
 }
